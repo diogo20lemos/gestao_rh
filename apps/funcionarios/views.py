617770d0430945599077579806b353558f76dd7b
Cicero Diogo Lemos <diogolemos@EDiagnose.local>
2020-08-28 13:45:48 -0300

    Aula 31 Listagem de Registro de Hora Extra

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView)
+from .models import Funcionario
 
-# Create your views here.
+
+class FuncionariosList(ListView):
+    model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
+
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamento']
+
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('list_funcionarios')
+
+
+class FuncionarioNovo(CreateView):
+    model = Funcionario
+    fields = ['nome', 'departamento']
+
+    def form_valid(self, form):
+        funcionario = form.save(commit=False)
+        username = funcionario.nome.split(' ')[0] + \
+                   funcionario.nome.split(' ')[1]
+        funcionario.empresa = self.request.user.funcionario.empresa
+        funcionario.user = User.objects.create(username=username)
+        funcionario.save()
+        return super(FuncionarioNovo, self).form_invalid(form)
---
+++ apps/registro_hora_extra/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from .views import (
+    HoraExtraList,
+    # FuncionarioEdit,
+    # FuncionarioDelete,
+    # FuncionarioNovo
+)
+
+urlpatterns = [
+    path('', HoraExtraList.as_view(), name='list_hora_extra'),
+    # path('novo/', FuncionarioNovo.as_view(), name='create_funcionario'),
+    # path('editar/<int:pk>/',
+    #         FuncionarioEdit.as_view(), name='update_funcionario'),
+    # path('delete/<int:pk>/',
+    #         FuncionarioDelete.as_view(), name='delete_funcionario'),
+
+ ]
