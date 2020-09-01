3ba1af8872c21879714efde29c4e9eb34620acdf
Cicero Diogo Lemos <diogolemos@EDiagnose.local>
2020-09-01 12:46:48 -0300

    terminado o CRUD

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .models import RegistroHoraExtra
-from django.views.generic import ListView
+from django.urls import reverse_lazy
 
+from .models import RegistroHoraExtra
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+
+from .forms import RegistroHoraExtraForm
 
 class HoraExtraList(ListView):
     model = RegistroHoraExtra
@@ -11,3 +14,28 @@ class HoraExtraList(ListView):
         return RegistroHoraExtra.objects.filter(
             funcionario__empresa=empresa_logada
         )
+
+
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraNovo, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('list_hora_extra')
+
+
+class HoraExtraNovo(CreateView):
+    model = RegistroHoraExtra
+    form_class = RegistroHoraExtraForm
+
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraNovo, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
