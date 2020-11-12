b855d303733651d70dcb4b7348572dfe32bb80cf
Cicero Diogo Lemos <diogolemos@EDiagnose.local>
2020-11-12 13:41:22 -0300

    melhorando a tela inicial

--- apps/core/views.py
+++ apps/core/views.py
@@ -13,6 +13,12 @@ from .tasks import send_relatorio
 @login_required
 def home(request):
     data = {'usuario': request.user}
+    funcionario = request.user.funcionario
+    data['total_funcionarios'] = funcionario.empresa.total_funcionarios
+    data['total_funcionarios_ferias'] = \
+        funcionario.empresa.total_funcionarios_ferias
+    data['total_funcionarios_doc_pendente'] =\
+        funcionario.empresa.total_funcionarios_doc_pendente
     return render(request, 'core/index.html', data)
 
 
